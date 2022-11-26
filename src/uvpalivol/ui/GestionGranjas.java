67f776a42791f423f8d4923a8c86520953a52511
RYZEN3 <RYZEN3@Catalina>
2022-11-25 19:24:49 -0500

    Prueba NUMERO 1

--- src/uvpalivol/ui/GestionGranjas.java
+++ src/uvpalivol/ui/GestionGranjas.java
@@ -1,19 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package uvpalivol.ui;
 
-/**
- *
- * @author RYZEN3
- */
-public class GestionGranjas extends javax.swing.JFrame {
+import uvpavicol.clases.Empresa;
 
+
+public class GestionGranjas extends javax.swing.JInternalFrame {
+private Empresa empresa;
     /**
      * Creates new form GestionGranjas
      */
-    public GestionGranjas() {
+    public GestionGranjas(Empresa empresa) {
+        this.empresa = empresa;
         initComponents();
     }
 
@@ -532,40 +529,6 @@ public class GestionGranjas extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GestionGranjas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GestionGranjas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GestionGranjas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GestionGranjas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GestionGranjas().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
--- src/uvpavicol/main/UVPavicol.java
+++ src/uvpavicol/main/UVPavicol.java
@@ -1,12 +1,17 @@
 package uvpavicol.main;
 
+import uvpalivol.ui.Start;
+import uvpavicol.clases.Empresa;
+
 
 public class UVPavicol {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Empresa UVpavicol = new Empresa ("UVpavicol");
+        new Start(UVpavicol).setVisible(true);
     }
     
 }
